a031864f1f6964555c5039a6241939118b581acf
spencer <spencer602@gmail.com>
2019-05-23 21:56:06 -0600

    Initial Commit

---
+++ Assignment 1: Concentration/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Assignment 1: Concentration
+//
+//  Created by Spencer DeBuf on 5/23/19.
+//  Copyright © 2019 Spencer DeBuf. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
